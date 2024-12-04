4e26bb9ffa13b5161553e7a9e8fd101f8d3d151e
RRevkoUA <rustamanrevko@gmail.com>
2024-12-04 20:51:35 +0200

    fix fuse

--- src/http_get.cpp
+++ src/http_get.cpp
@@ -23,6 +23,7 @@ int8_t http_download(const std::string url, const char *const output, const outp
             std::cerr << "Error: File already exists." << std::endl;
             return -1;
         }
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
         curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1L);
 
@@ -37,7 +38,8 @@ int8_t http_download(const std::string url, const char *const output, const outp
         file = fopen(output_file.c_str(), output_enum_map[output_type].c_str());
         if (file)
         {
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_NOBODY, 0L);
+            curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 0L);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
             res = curl_easy_perform(curl);
             if (res != CURLE_OK)
@@ -64,17 +66,23 @@ int8_t http_download(const std::string url, const char *const output, const outp
 
 static int8_t download_prepare(std::string *output_file, const std::string url, const char *const output, const output_enum_t output_type)
 {
+    std::string temp_url = url;
+    
+    if (temp_url.back() == '/') {
+        temp_url.pop_back();
+    }
+
     std::cout<<"Preparing to download file from: "<<url<<std::endl;
     if (output == nullptr) {
         *output_file = "./";
-        *output_file += url.substr(url.find_last_of("/") + 1);
+        *output_file += temp_url.substr(temp_url.find_last_of("/") + 1);
         std::cout<<"Output file not specified. Using default: "<<*output_file<<std::endl;
 
     } 
     else if (std::filesystem::is_directory(output)) {
         *output_file = output;
         *output_file += "/";
-        *output_file += url.substr(url.find_last_of("/") + 1);
+        *output_file += temp_url.substr(temp_url.find_last_of("/") + 1);
     }
     else {
         *output_file = output;
